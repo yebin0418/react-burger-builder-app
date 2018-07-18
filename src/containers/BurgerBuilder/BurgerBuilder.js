f911092c416704e65fc0fbff7f109d03f899d998
Yebin Park <yb507970@dal.ca>
2018-07-18 20:40:14 -0300

    Displaying and Updating the Burger Price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,15 +38,38 @@ class BurgerBuilder extends Component{
     const newPrice = oldPrice + priceAddition;
     this.setState({totalPrice: newPrice, ingredients: updateIngredients});
   }
+
   removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if(oldCount <= 0){
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updateIngredients = {
+      ...this.state.ingredients
+    };
+    updateIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalPrice: newPrice, ingredients: updateIngredients});
   }
 
   render(){
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for(let key in disabledInfo){
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
     return(
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
-          ingredientAdded = {this.addIngredientHandler}/>
+          ingredientAdded = {this.addIngredientHandler}
+          ingredientRemoved = {this.removeIngredientHandler}
+          disabled={this.disabledInfo}
+          price ={this.state.totalPrice}/>
       </Aux>
     );
   }
