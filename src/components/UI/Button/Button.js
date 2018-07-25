fce6122354d2c463318f0a582b0689b708d08848
yebin Park <yb507970@dal.ca>
2018-07-25 19:47:05 -0300

    updated Button.js

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,6 +3,7 @@ import classes from './Button.css';
 
 const button = (proprs) => (
   <button
+    className={[classes.Button, classes[props.btnType]].join(' ')}
     onClick={props.clicked}>{props.children}</button>
 );
 
