69cfd8cfca19b805464c13cee2734baf0b49a01d
yebin Park <yb507970@dal.ca>
2018-07-30 21:46:34 -0300

    added css style for navigation

--- src/components/Navigation/NavigationItems/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import classes from'./NavigationItem.css';
 
 const navigationItem = (props) => (
-  <ul>
-    <li><a href="/">A Link</a></li>
-  </ul>
+
+    <li className= {classes.NavigationItem}>
+      <a href={props.link}
+        className={props.active ? classes.active : null}>{props.children}
+      </a>
+    </li>
+
 );
 
 export default navigationItem;
